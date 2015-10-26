6edab917a94ce951caaf0db9470e752f7da4633e
melissathompson24 <melissa.thompson24@gmail.com>
2015-10-25 20:05:21 -0600

    add all files

---
+++ js/load_essentials_footer.js
@@ -0,0 +1,14 @@
+//footer
+
+
+document.getElementById("myFooter").innerHTML =
+  "<div class='container-fluid text-center'>"
+  +  "<p class='text-muted'><a href='index.html'>Novak Law LLC</a></p>"
+  +  "<p class='text-muted'><a href='about.html'>About</a> | <a href='contact.html'>Contact.html</a> | <a href='practice.html</a>Practice Areas</a> | <a href='attorney.html'>Attorney</a> | <a href='resources.html'>Resources & Articles</a></p>"
+  +  "<p class='text-muted'>Copyright &copy; " 
+  +  new Date().getFullYear()
+  +  ". All Rights Reserved.</span></p>"
+  ;
+
+
+
---
+++ js/load_essentials_header.js
@@ -0,0 +1,70 @@
+// navigation 
+
+document.getElementById("myNav").innerHTML =
+  "<div class='container-fluid'>"
+  + "<div class='navbar-header'>"
+  +  "<a class='navbar-brand' href='index.html'>"
+  +  "<img class='img-responsive center-block' alt='Logo stating the name Novak Law LLC: Real Estate and Business Law across the Denver Front Range.' src='./images/siteID_novak.png'>"
+  +  "</a>"
+  +  "</div>"
+
+  +  "<a href='#skipToContent'>"
+  +  "<img class='img-responsive' alt='Skip to main content' src='./images/skipToContent.png'>"
+  +  "</a>"
+
+  
+  +  "<div class='navbar-right nav_line1'>"
+  +  "<a href=sitemap.html' class='padding-halfem margin-r-1em'>Sitemap</a>"
+  +  "<input id='search' name='search' type='text' required placeholder='Search'>"
+  +  "<button type=button' class='btn btn-default margin1em' aria-label='Search'>"
+  +  "<span class='glyphicon glyphicon-search' aria-hidden='true'></span>"
+  +  "</button>"
+  +  "</div>"
+
+
+
+  +  "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>"
+  +  "<span class='sr-only'>Toggle navigation</span>"
+  +  "<span class='icon-bar'></span>"
+  +  "<span class='icon-bar'></span>"
+  +  "<span class='icon-bar'></span>"
+  +  "</button>"
+  +  "</div>"
+  +  "<div id='navbar' class='collapse navbar-collapse  navbar-right margin-r-1em'>"
+  +  "<ul class='nav navbar-nav'>"
+  +  "<li><a href='index.html'>Home</a></li>"
+  +  "<li><a href='about.html'>About</a></li>"
+  +  "<li><a href='contact.html'>Contact</a></li>"
+  +  "<li class='dropdown dropdown-menu-right'>"
+  +  "<a href='practice.html' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Practice Areas<span class='caret'></span></a>"
+  +  "<ul class='dropdown-menu'>"
+  +  "<li><a href='realEstate.html'>Real Estate Law</a></li>"
+  +  "<li><a href='businessFormation.html'>Business Formation</a></li>"
+  +  "<li><a href='businessDisputes.html'>Business Disputes</a></li>"
+  +  "<li><a href='appellate.html'>Appellate Litigation</a></li>"
+  +  "<li><a href='mediation.html'>Mediation</a></li>"
+  +  "</ul>"
+  +  "</li>"
+  +  "<li><a href='attorney.html'>Attorney</a></li>"
+  +  "<li><a href='resources.html'>Resources & Articles</a></li>"
+  +  "<ul class='nav navbar-nav'>"
+  +  "</ul>"
+  +  "</div><!--/.nav-collapse -->"
+  +  "</div>"
+  ;
+
+
+
+
+
+document.getElementById("myFooter").innerHTML =
+  "<div class='container-fluid text-center'>"
+  +  "<p class='text-muted'><a href='index.html'>Novak Law LLC</a></p>"
+  +  "<p class='text-muted'><a href='about.html'>About</a> | <a href='contact.html'>Contact.html</a> | <a href='practice.html</a>Practice Areas</a> | <a href='attorney.html'>Attorney</a> | <a href='resources.html'>Resources & Articles</a></p>"
+  +  "<p class='text-muted'>Copyright &copy; " 
+  +  new Date().getFullYear()
+  +  ". All Rights Reserved.</span></p>"
+  ;
+
+
+
