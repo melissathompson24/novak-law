cdc5a3bb4946bbd71a5f0d2eb55ae6c058363ce4
melissathompson24 <melissa.thompson24@gmail.com>
2015-11-05 20:44:24 -0700

    updated footer

--- js/load_essentials_footer.js
+++ js/load_essentials_footer.js
@@ -4,7 +4,7 @@
 document.getElementById("myFooter").innerHTML =
   "<div class='container-fluid text-center'>"
   +  "<p class='text-muted'><a href='index.html'>Novak Law LLC</a></p>"
-  +  "<p class='text-muted'><a href='about.html'>About</a> | <a href='contact.html'>Contact</a> | <a href='practice.html'</a>Practice Areas</a> | <a href='attorney.html'>Attorney</a> | <a href='resources.html'>Resources & Articles</a></p>"
+  +  "<p class='text-muted'><a href='about.html'>About</a> | <a href='contact.html'>Contact</a> | <a href='legal.html'</a>Legal Services</a> | <a href='mediation.html'>Mediation Services</a> | <a href='resources.html'>Resources & Articles</a></p>"
   +  "<p class='text-muted'>Site Design by Melissa Thompson. Copyright &copy; " 
   +  new Date().getFullYear()
   +  ". All Rights Reserved.</span></p>"
